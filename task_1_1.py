0d440dcf967d9f435309dcaaeb97b86ea2af3f4c
Task_1_1 <makhortova.sofia@gmai;.com>
2024-04-26 02:16:41 +0300

    Task 2

---
+++ task_1_1.py
@@ -0,0 +1,28 @@
+x = float(input())
+y = float(input())
+## 1
+# print(x+y, x*y)
+## 2
+# print(max(x+y, x-y, x*y, x/y, x//y))
+## 3
+a=0
+b=0
+if x+y>x-y:
+    a=x+y
+if x*y>x/y:
+    b=x*y
+c =  x//y
+if a>b>c:
+    mid=b
+elif c>b>a:
+    mid=b
+elif a>c>b:
+    mid=c
+elif c>a>b:
+    mid=a
+elif b>a>c:
+    mid=a
+elif b>c>a:
+    mid=c
+
+print(mid)
